25acc5d86f6bbbab621e208eecc3566b2181f94e
rlelisferrari <rlelisferrari@gmail.com>
2021-04-09 22:12:45 -0300

    add novo controller. remove controller default

---
+++ WebAppDemo/WebAppDemo/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] {"teste", "teste2"};
+        }
+
+        [HttpGet("pessoas")]
+        public ActionResult<IEnumerable<Pessoa>> GetPessoas()
+        {
+            return new[]
+            {
+                new Pessoa {Nome = "Rafael"},
+                new Pessoa {Nome = "Rafaela"}
+            };
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome;
+    }
+}
